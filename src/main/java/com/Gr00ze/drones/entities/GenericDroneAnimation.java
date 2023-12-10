0bfe248bbc9c06e08f336e01f51fe8d4afd7b422
gr00ze <vincenzop23@gmail.com>
2023-12-10 18:55:56 +0100

    Added first Animation

---
+++ src/main/java/com/Gr00ze/drones/entities/GenericDroneAnimation.java
@@ -0,0 +1,17 @@
+package com.Gr00ze.drones.entities;
+
+import net.minecraft.client.animation.AnimationChannel;
+import net.minecraft.client.animation.AnimationDefinition;
+import net.minecraft.client.animation.Keyframe;
+import net.minecraft.client.animation.KeyframeAnimations;
+
+public class GenericDroneAnimation {
+    public static final AnimationDefinition animation = AnimationDefinition.Builder.withLength(1F).looping()
+            .addAnimation("rotore", new AnimationChannel(AnimationChannel.Targets.ROTATION,
+                    new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(0.5F, KeyframeAnimations.degreeVec(0.0F, -180.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(1.0F, KeyframeAnimations.degreeVec(0.0F, -360.0F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+            ))
+            .build();
+
+}
