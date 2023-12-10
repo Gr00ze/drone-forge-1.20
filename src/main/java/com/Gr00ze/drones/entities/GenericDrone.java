69901ec6e05bbf225ebbe6aad7ef880b57b970de
gr00ze <vincenzop23@gmail.com>
2023-12-10 19:11:17 +0100

    Removed unused imports

--- src/main/java/com/Gr00ze/drones/entities/GenericDrone.java
+++ src/main/java/com/Gr00ze/drones/entities/GenericDrone.java
@@ -1,18 +1,18 @@
 package com.Gr00ze.drones.entities;
 
-import com.google.common.eventbus.Subscribe;
+
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.MoverType;
+
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.Vec3;
+
 import org.jetbrains.annotations.NotNull;
 
 
