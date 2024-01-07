5b3373a60a0d536bf0dcc0c4f35fca1cba8ad911
Gr00ze <vincenzop23@gmail.com>
2024-01-07 21:20:08 +0100

    removed unused imports

--- src/main/java/com/Gr00ze/drones/entities/GenericDrone.java
+++ src/main/java/com/Gr00ze/drones/entities/GenericDrone.java
@@ -16,7 +16,6 @@ import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 
-import net.minecraft.world.entity.vehicle.Boat;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
