696a2691f51d1ede74d1bd3e492407106111a92d
gr00ze <vincenzop23@gmail.com>
2024-01-21 22:06:48 +0100

    Added forge event handler for debug

---
+++ src/main/java/com/Gr00ze/drones/events/ForgeEvents.java
@@ -0,0 +1,25 @@
+package com.Gr00ze.drones.events;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import static com.Gr00ze.drones.DronesMod.MOD_ID;
+
+@Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class ForgeEvents {
+    @SubscribeEvent
+    public static void onEntityInteract(PlayerInteractEvent.EntityInteract event ){
+        Player player = event.getEntity();
+        Entity entity = event.getTarget();
+        ItemStack itemStack = player.getMainHandItem();
+        System.out.println("Tentativo cavalcaggio");
+        boolean isDrone = entity instanceof Entity;
+        if (isDrone && itemStack.isEmpty()){
+            player.startRiding(entity);
+        }
+    }
+}
