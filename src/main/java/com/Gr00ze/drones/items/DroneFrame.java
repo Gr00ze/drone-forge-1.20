129ae0a01a51a2017263c8ba1c6b36a07633a5e2
gr00ze <vincenzop23@gmail.com>
2024-01-28 21:19:08 +0100

    Added items and creative tab

---
+++ src/main/java/com/Gr00ze/drones/items/DroneFrame.java
@@ -0,0 +1,12 @@
+package com.Gr00ze.drones.items;
+
+import net.minecraft.world.item.Item;
+
+public class DroneFrame extends ItemVoid {
+
+
+    public void useAction() {
+
+
+    }
+}
---
+++ src/main/java/com/Gr00ze/drones/items/Motor.java
@@ -0,0 +1,7 @@
+package com.Gr00ze.drones.items;
+
+import net.minecraft.world.item.Item;
+
+public class Motor extends ItemVoid {
+
+}
